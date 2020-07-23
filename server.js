d6dc511bde7e120e3e99e7b26b824de10075b335
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-07-23 15:39:55 +0530

    handeling requests

--- server.js
+++ server.js
@@ -15,21 +15,21 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const Schema = mongoose.Schema;
 
-const logInfo = new Schema({
-  description: { type: String },
-  duration: { type: Number },
+const logInfoSchema = new Schema({
+  description: { type: String, required: true, default: "" },
+  duration: { type: Number, required: true, default: "" },
   date: { type: String }
 });
 
-const userInfo = new Schema({
+const userInfoSchema = new Schema({
   username: { type: String, required: true, unique: true },
   from: { type: String },
   to: { type: String },
   count: { type: Number },
-  log: [logInfo]
+  log: [logInfoSchema]
 });
 
-
+const userInfo = mongoose.model("userInfo", userInfoSchema);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -40,12 +40,77 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// Not found middleware
-app.use((req, res, next) => {
-  return next({ status: 404, message: "not found" });
+app.post("/api/exercise/new-user", (req, res) => {
+  const username = req.body.username;
+
+  console.log(username);
+  const newUser = new userInfo({ username: username });
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.send("h");
+  });
+
+  userInfo.find({}, (err, data) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      console.log("hi");
+      res.send("h");
+      console.log(data);
+    }
+  });
 });
 
-// Error Handling middleware
+app.post("/api/exercise/new-user", (req, res) => {
+  const userId = req.body.userId;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  let date;
+
+  if (req.body.date) {
+    date = req.body.date;
+  } else {
+    date = String(new Date()).substring(0, 15);
+  }
+
+  userInfo.findById(userId, function (err, user) {
+    if (err) throw err;
+    userInfo.log.push({
+      description: description,
+      duration: duration,
+      date: date
+    });
+    userInfo.save(function (err, user) {
+      if (err) console.error(err);
+    });
+  });
+});
+
+app.get("/api/exercise/log?userId&from&to&limit", (req, res) => {
+  const userId = req.query.userId;
+  if (!userId) {
+    res.send("enter user id");
+  } else {
+    userInfo.find({ userId: userId }, (err, data) => {
+      if (err) {
+        return err;
+      } else {
+        res.json(data);
+      }
+    });
+  }
+});
+
+// Not found middleware
+
+// app.use((req, res, next) => {
+//   return next({ status: 404, message: "not found" });
+// });
+
+//Error Handling middleware
 app.use((err, req, res, next) => {
   let errCode, errMessage;
 
@@ -60,7 +125,10 @@ app.use((err, req, res, next) => {
     errCode = err.status || 500;
     errMessage = err.message || "Internal Server Error";
   }
-  res.status(errCode).type("txt").send(errMessage);
+  res
+    .status(errCode)
+    .type("txt")
+    .send(errMessage);
 });
 
 const listener = app.listen(process.env.PORT, () => {
